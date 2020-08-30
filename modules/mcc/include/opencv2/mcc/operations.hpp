9d2961c538bb54b08656b5ae0ed31a58ef393aed
shanchenqi <shanchenqi@huawei.com>
2020-08-30 11:14:23 +0800

    notes

--- modules/mcc/include/opencv2/mcc/operations.hpp
+++ modules/mcc/include/opencv2/mcc/operations.hpp
@@ -1,6 +1,6 @@
 // This file is part of OpenCV project.
-// It is subject to the license terms in the LICENSE file found in the top-level
-// directory of this distribution and at http://opencv.org/license.html.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
 
 /*
  * MIT License
@@ -14,8 +14,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -31,94 +31,112 @@
 
 #include <functional>
 #include <vector>
-
 #include "opencv2/mcc/utils.hpp"
 
-namespace cv {
-namespace ccm {
+namespace cv 
+{
+namespace ccm 
+{
 
 typedef std::function<cv::Mat(cv::Mat)> MatFunc;
 
 /* brief Operation class contains some operarions used for color space
- *       conversion containing linear transformation and non-linear
- * transformation
+ *       conversion containing linear transformation and non-linear transformation
  */
-class Operation {
- public:
-  bool linear;
-  cv::Mat M;
-  MatFunc f;
+class Operation 
+{
+public:
+    bool linear;
+    cv::Mat M;
+    MatFunc f;
 
-  Operation() : linear(true), M(cv::Mat()){};
+    Operation() : linear(true), M(cv::Mat()) {};
 
-  Operation(cv::Mat M) : linear(true), M(M){};
+    Operation(cv::Mat M) :linear(true), M( M ) {};
 
-  Operation(MatFunc f) : linear(false), f(f){};
+    Operation(MatFunc f) : linear(false), f(f) {};
 
-  virtual ~Operation(){};
-  /**\brief operator function will run operation*/
-  cv::Mat operator()(cv::Mat& abc) {
-    if (!linear) {
-      return f(abc);
-    }
-    if (M.empty()) {
-      return abc;
-    }
-    return multiple(abc, M);
-  };
+    virtual ~Operation() {};
+    /**\brief operator function will run operation*/
+    cv::Mat operator()(cv::Mat& abc) 
+    {
+        if (!linear) 
+        { 
+            return f(abc); 
+        }
+        if (M.empty()) 
+        { 
+            return abc; 
+        }
+        return multiple(abc, M);
+    };
 
-  /**\brief add function will conbine this operation with  other  linear
-   *        transformation operation*/
-  void add(const Operation& other) {
-    if (M.empty()) {
-      M = other.M.clone();
-    } else {
-      M = M * other.M;
-    }
-  };
+    /**\brief add function will conbine this operation 
+     *        with  other  linear transformation operation*/
+    void add(const Operation& other) 
+    {
+        if (M.empty()) 
+        {
+            M = other.M.clone();
+        }
+        else 
+        {
+            M = M * other.M;
+        }
+    };
 
-  void clear() { M = cv::Mat(); };
+    void clear() 
+    {
+        M = cv::Mat();
+    };
 };
 
-const Operation IDENTITY_OP([](cv::Mat x) { return x; });
+const Operation IDENTITY_OP( [](cv::Mat x) {return x; } );
 
-class Operations {
- public:
-  std::vector<Operation> ops;
+class Operations 
+{
+public:
+    std::vector<Operation> ops;
 
-  Operations() : ops{} {};
+    Operations() :ops{ } {};
 
-  Operations(std::initializer_list<Operation> op) : ops{op} {};
+    Operations(std::initializer_list<Operation> op) :ops{ op } {};
 
-  virtual ~Operations(){};
+    virtual ~Operations() {};
 
-  /**\brief add function will conbine this operation with  other  transformation
-   * operations*/
-  Operations& add(const Operations& other) {
-    ops.insert(ops.end(), other.ops.begin(), other.ops.end());
-    return *this;
-  };
+    /**\brief add function will conbine this operation with  other  transformation operations*/
+    Operations& add(const Operations& other) 
+    {
+        ops.insert(ops.end(), other.ops.begin(), other.ops.end());
+        return *this;
+    };
 
-  /**\brief run operations to make color conversion*/
-  cv::Mat run(cv::Mat abc) {
-    Operation hd;
-    for (auto& op : ops) {
-      if (op.linear) {
-        hd.add(op);
-      } else {
+    /**\brief run operations to make color conversion*/
+    cv::Mat run(cv::Mat abc) 
+    {
+        Operation hd;
+        for (auto& op : ops) 
+        {
+            if (op.linear) 
+            {
+                hd.add(op);
+            }
+            else 
+            {
+                abc = hd(abc);
+                hd.clear();
+                abc = op(abc);
+            }
+        }
         abc = hd(abc);
-        hd.clear();
-        abc = op(abc);
-      }
-    }
-    abc = hd(abc);
-    return abc;
-  };
+        return abc;
+    };
 };
 
-const Operations IDENTITY_OPS{IDENTITY_OP};
+const Operations IDENTITY_OPS{ IDENTITY_OP };
 
-}  // namespace ccm
-}  // namespace cv
+} // namespace ccm
+} // namespace cv
+
 
 #endif
